0b8cfc6457e736ba26c767ff834db7d6941b7cbe
LowieDown <11574551+LowieDown@users.noreply.github.com>
2017-09-06 14:10:59 -0400

    Changed cookie separator back to a semicolon

--- src/android/com/synconset/CordovaHTTP/CordovaHttpGet.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpGet.java
@@ -47,7 +47,7 @@ public class CordovaHttpGet extends CordovaHttp implements Runnable {
             String[] cookies = request.headers("Set-Cookie");
             for (int i = 0; i < cookies.length; i++) {
                 if (i > 0) {
-                    sb.append(",");
+                    sb.append(";");
                 }
                 String item = cookies[i];
                 sb.append(item);
