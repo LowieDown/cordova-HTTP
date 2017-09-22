a5a555a8db79918e4b1501ff9ac0ab8fc5ce358c
LowieDown <11574551+LowieDown@users.noreply.github.com>
2017-09-22 13:14:15 -0400

    set cookies

--- src/android/com/synconset/CordovaHTTP/CordovaHttpPost.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpPost.java
@@ -43,6 +43,10 @@ public class CordovaHttpPost extends CordovaHttp implements Runnable {
                     sb.append(";");
                 }
                 String item = cookies[i];
+                String[] parts = item.split("=", 2);
+                String cookieName = parts[0];
+                String cookieValue = parts[1];
+                this.setCookie(cookieName, cookieValue);
                 sb.append(item);
             }
             response.put("cookie", sb.toString());
