f82eb9e7fb30a7bb48f05ef8efa0eb38255868a4
LowieDown <11574551+LowieDown@users.noreply.github.com>
2018-01-24 15:58:46 -0500

    Update CordovaHttpPlugin.m

--- src/ios/CordovaHttpPlugin.m
+++ src/ios/CordovaHttpPlugin.m
@@ -107,7 +107,7 @@
         NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
         [self setResults: dictionary withTask: task];
         [dictionary setObject:responseObject forKey:@"data"];
-        [dictionary setObject:responseObject forKey:@"headers"];
+        [dictionary setObject:responseObject forKey:@"allHeaderFields"];
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:dictionary];
         [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     } failure:^(NSURLSessionTask *task, NSError *error) {
