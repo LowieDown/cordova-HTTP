95aa3f1d8ee21f5a28007fade7393d473a31f75a
LowieDown <11574551+LowieDown@users.noreply.github.com>
2018-01-26 13:45:22 -0500

    Update CordovaHttpPlugin.m

--- src/ios/CordovaHttpPlugin.m
+++ src/ios/CordovaHttpPlugin.m
@@ -77,7 +77,7 @@
     [self setRequestHeaders: headers forManager: manager];
    
     CordovaHttpPlugin* __weak weakSelf = self;
-    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+
     manager.responseSerializer = [TextResponseSerializer serializer];
     NSLog(@"%@", parameters);
     NSLog(@"%@", headers);
@@ -108,6 +108,8 @@
     CordovaHttpPlugin* __weak weakSelf = self;
    
     manager.responseSerializer = [TextResponseSerializer serializer];
+    NSLog(@"%@", parameters);
+    NSLog(@"%@", headers);
     [manager GET:url parameters:parameters progress:nil success:^(NSURLSessionTask *task, id responseObject) {
         NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
         [self setResults: dictionary withTask: task];
